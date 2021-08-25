a6e3cd5f17c41b766501740c36d20a2e5f6ecf9b
m-v-smirnov <m.v.smirnoff86@yandex.ru>
2021-08-25 10:57:42 +0300

    New : insert random square, get array of pos of Zero elements, new game function, colors in render

--- game_script.js
+++ game_script.js
@@ -2,9 +2,68 @@
 
 let gameArr = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
 let fieldElem = document.getElementById('table');
+let squareColors = {
+    0: 'lightgrey',
+    2: 'aquamarine',
+    4: 'aqua',
+    8: 'chartreuse',
+    16: 'cornflowerblue',
+    32: 'chocolate',
+    64: 'crimson',
+    128: 'orange',
+    256: 'yellow',
+    512: 'blue',
+    1024: 'red',
+    2048: 'greenyellow',
+}
 //gameArr.forEach(function(item, index, array) {
 //    console.log(item);
 //});
+
+function fieldRender(arr,elem) {
+    if (!arr || !elem) return;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[0].length; j++) {
+            let item = arr[i][j];
+            elem.rows[j].cells[i].style.backgroundColor = squareColors[item];
+            if (item == 0) item = '';
+            elem.rows[j].cells[i].innerHTML = item;
+            
+        }
+    }
+};
+
+function getZerosPos(arr) {
+    let result = [];
+    let item;
+    if (!arr) return result;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[0].length; j++) {
+            item = arr[i][j];
+            if (item == 0) result.push([i,j]);
+        }
+    }
+    return result;
+}
+
+function insertRandomSquare(arr,item) {
+    let pos = getZerosPos(arr);
+    let index = Math.floor(Math.random() * pos.length);
+    arr[pos[index][0]][pos[index][1]] = item;
+}
+
+function newGame() {
+    if (!gameArr) return result;
+    for (let i = 0; i < gameArr.length; i++) {
+        for (let j = 0; j < gameArr[0].length; j++) {
+            gameArr[i][j] = 0;
+        }
+    }
+    insertRandomSquare(gameArr,2);
+    insertRandomSquare(gameArr,2);
+    fieldRender(gameArr,fieldElem);
+}
+
 
 document.addEventListener('keydown', function(event) {
     switch (event.code) {
@@ -25,18 +84,27 @@ document.addEventListener('keydown', function(event) {
             break;
     }
 });
+document.addEventListener('click', function(event) {
+    if (event.target.dataset.start != undefined) {
+        newGame();
 
-function fieldRender(arr,elem) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[0].length; j++) {
-            fieldElem.rows[i].cells[j].innerHTML = arr[j][i];
-        }
     }
-};
+});
 
-//gameArr[2][1] = 1024;
-//gameArr[3][3] = 32;
+insertRandomSquare(gameArr,2);
+insertRandomSquare(gameArr,4);
+insertRandomSquare(gameArr,8);
+insertRandomSquare(gameArr,16);
+insertRandomSquare(gameArr,32);
+insertRandomSquare(gameArr,64);
+insertRandomSquare(gameArr,128);
+insertRandomSquare(gameArr,256);
+insertRandomSquare(gameArr,512);
+insertRandomSquare(gameArr,1024);
+insertRandomSquare(gameArr,2048);
+//newGame();
 
+//console.log(getZerosPos(gameArr));
 //gameArr.forEach(function(item, index, array) {
 //    console.log(item);
 //});
